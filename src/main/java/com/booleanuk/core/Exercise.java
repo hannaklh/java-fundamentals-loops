2372a2cc11887cf3c60c737b83e8ba2a4012c195
hannaklh <hannaklh@gmail.com>
2025-08-12 16:21:12 +0200

    exercise complete

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -11,34 +11,53 @@ public class Exercise {
 
     public void stepOne() {
         // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
-
+        numsZeroToThree = new int[4];
+        for (int i = 0; i < 4; i++) {
+            numsZeroToThree[i] = i;
+        }
 
     }
 
     public void stepTwo() {
         // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
+        numsFiveToTen = new int[6];
+        for (int i = 0; i < 6; i++) {
+            numsFiveToTen[i] = i+5;
+        }
 
 
     }
 
     public void stepThree() {
         // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
+        countdown = new int[4];
+        for (int i = 0; i < 4; i++) {
+            countdown[i] = 3-i;
 
+        }
 
     }
 
     public boolean stepFour(int num) {
         // TODO: 6. Write a for loop that checks if num is in the favouriteNumbers array
+        for (int element : favouriteNumbers) {
+            if (element == num) {
+                return true;
+            }
 
-
-
+        }
         return false;
+
+
     }
 
     public boolean stepFive(String hobby) {
         // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-
-
+        for (String object : myHobbies) {
+            if (object.equals(hobby)) {
+                return true;
+            }
+        }
 
         return false;
     }
